1267b374c9c9c3b728ecfb1dacafd200b11e32cf
Prakash Rakshit <prakashrakshit698@gmail.com>
2024-10-18 18:04:46 +0530

    fixed error

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
     // ...
     upload: {
@@ -15,5 +17,11 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    // ...
+    'users-permissions': {
+    config: {
+      jwt: {
+        jwtSecret: env('STRAPI_JWT_SECRET') || crypto.randomBytes(16).toString('base64')
+      },
+    },
+  },
   });
